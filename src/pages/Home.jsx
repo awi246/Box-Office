130013bb030c42e17b9e81c8df2751a1ef70b9c5
Awiral <awiral1234@gmail.com>
2023-07-23 15:30:38 +0545

    route

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { Link } from "react-router-dom";
+const Home = () =>{
+    // return<div>hii this is home page</div>
+    return<Link to="starred">Go to Starred page</Link>
+};
+export default Home;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+const Starred = () => {
+    // return ("eufhijklqvef");
+    return <Link to="constact">go to contect </Link>
+};
+
+export default Starred;
